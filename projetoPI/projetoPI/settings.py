6a577eb58ede99d24a6834b9d7403da52dc16a9c
EvertonMachado1 <106996058+EvertonMachado1@users.noreply.github.com>
2025-03-27 20:50:54 +0000

    instalado o mysql e o phpmyadmin. configurado os databases do settings.py

--- projetoPI/projetoPI/settings.py
+++ projetoPI/projetoPI/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "projetoPI.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'projeto',
+        'USER': 'root',
+        'PASSWORD': 'univesp2025',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
